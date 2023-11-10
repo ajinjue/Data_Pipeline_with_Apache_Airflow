c6f34dcf3021d534c0437911b8c8749953c77cf7
AKWAYAGA  COLLINUS AJINJUE <collinusakwayaga@gmail.com>
2023-11-10 12:07:01 +0100

    Updated the Dimension Operator

--- Airflow/plugins/operators/load_dimension.py
+++ Airflow/plugins/operators/load_dimension.py
@@ -21,24 +21,28 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id = "",
                  table = "",
                  sql = "",
+                 append_mode = True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         # Map params
         self.redshift_conn_id = redshift_conn_id
         self.table = table
-        self.sql = sql
+        self.sql = sql,
+        self.append_mode = append_mode
 
     def execute(self, context):
-        self.log.info('Connecting to Redshift and Inserting Data')
+        self.log.info('Connecting to Redshift to Insert Data')
         redshift_hook = PostgresHook("redshift")
         
-        self.log.info(f"Truncating dimension table: {self.table}")
-        redshift_hook.run(LoadDimensionOperator.truncate_sql.format(self.table))
+        if self.append_mode:
+            self.log.info(f"Inserting Data to {self.table} ")
+            load_sql = LoadDimensionOperator.insert_sql.format(
+                self.table,
+                self.sql
+            )
+            redshift_hook.run(load_sql)
+        else:
+            self.log.info(f"Truncating dimension table: {self.table}")
+            redshift_hook.run(LoadDimensionOperator.truncate_sql.format(self.table))
         
-        load_sql = LoadDimensionOperator.insert_sql.format(
-            self.table,
-            self.sql
-        )
-        self.log.info(f"Inserting Data to {self.table} ")
-        redshift_hook.run(load_sql)
